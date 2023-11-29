e99ca78dbb779a6438758114eff3bd256eee6874
JorisRobben <149020196+JorisRobben@users.noreply.github.com>
2023-11-29 22:53:44 +0100

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,10 +1,10 @@
 import random
 import time
 
-HEARTS   = chr(9829) # Character 9829 is '♥'.
-DIAMONDS = chr(9830) # Character 9830 is '♦'.
-SPADES   = chr(9824) # Character 9824 is '♠'.
-CLUBS    = chr(9827) # Character 9827 is '♣'.
+HEARTS   = chr(9829) #Character 9829 is '♥'.
+DIAMONDS = chr(9830) #Character 9830 is '♦'.
+SPADES   = chr(9824) #Character 9824 is '♠'.
+CLUBS    = chr(9827) #Character 9827 is '♣'.
 POSSIBLE_SYMBOLS = [HEARTS, DIAMONDS, SPADES, CLUBS]
 
 def generate_card_deck():
