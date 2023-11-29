ed83b4999b725fe42cec8ac08d10aa928d66a271
JorisRobben <149020196+JorisRobben@users.noreply.github.com>
2023-11-29 22:52:45 +0100

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -54,125 +54,144 @@ def main():
   balance = 0
   print(f"Welcome to the blackjack table!\nYour balance is ${balance}.")
   while True:
+    #first loop. Only gets broken out of by pressing Q.
     player_move = input("What do you want to do?\nPress D to make a deposit, P to play, or Q to quit!\n>>> ")
     if player_move not in ("D", "P", "Q"):
       print("Please enter valid move.")
       continue
+      #continues back into the first loop until valid input is entered
     elif player_move == "Q":
       print(f"Thanks for playing!\nYour balance is ${balance}.")
       break
+      #breaks out of the first loop
     elif player_move == "D":
       deposit = input("How much would you like to deposit (max. $1000)\n>>> ")
       if deposit.isnumeric() and  0 < int(deposit) < 1000:
         balance = balance + int(deposit)
         print(f"Deposit accepted.\nYour balance is now ${balance}.")
         continue
+        #continues back into the first loop
       else:
         print("Please make a valid deposit.")
         continue
+        #continues back into the first loop
     elif player_move == "P":
-      while True:
-        bet = input(f"How much would you like to bet (max. ${balance})?\n>>> ")
-        if not bet.isnumeric() or not 0 < int(bet) <= balance:
-          print("Please make a valid bet.")
-          continue
-        else:
-          #Loop voor het trekken van de eerste kaarten
-          #1 voor dealer, 2 voor speler
-          card_deck = generate_card_deck()
-          dealer_cards = 0
-          dealer_points = 0
-          dealer_hand = {}
-          player_cards = 0
-          player_points = 0
-          player_hand = {}
-          time.sleep(1)
-          print ("The dealer is drawing cards...")
-          time.sleep(1)
-          #dealer draws its first card
-          dealer_cards += 1
-          dealer_hand[dealer_cards] = draw_card(card_deck)
-          dealer_points += dealer_hand[1][2]
-          #player draws its first card
-          player_cards += 1
-          player_hand[player_cards] = draw_card(card_deck)
-          player_points += player_hand[player_cards][2]
-          #player draws its second card
-          player_cards += 1
-          player_hand[player_cards] = draw_card(card_deck)
-          player_points += player_hand[player_cards][2]
-          #print and paint everything
-          print("Dealer:")
-          paint_card(dealer_hand[1][0], dealer_hand[1][1])
-          time.sleep(1)
-          print("You:")
-          paint_card(player_hand[1][0], player_hand[1][1])
-          time.sleep(1)
-          paint_card(player_hand[2][0], player_hand[2][1])
-          time.sleep(1)
-          print(f"\nThe dealer has {dealer_points} points, and you have {player_points} points.")
-          #next move for the player
-          next_move = input("What is your next move? D to draw, S to stop!\n>>> ")
+      if balance <= 0:
+        print("You don't have money on your balance.\nPlease make a deposit first.")
+        continue
+        #continues back in the first loop
+      else:
           while True:
-            #loop where the player can continue or stop playing
-            if next_move not in ("D", "S"):
-              next_move = input("Please enter a valid move! D to draw, S to stop!\n>>>")
+            #second loop, the loop of actually playing the game
+            bet = input(f"How much would you like to bet (max. ${balance})?\n>>> ")
+            if not bet.isnumeric() or not 0 < int(bet) <= balance:
+              print("Please make a valid bet.")
               continue
-            elif next_move == "D":
+            #continues back in the second loop until a valid bet is made
+            else:
+              card_deck = generate_card_deck()
+              dealer_cards = 0
+              dealer_points = 0
+              dealer_hand = {}
+              player_cards = 0
+              player_points = 0
+              player_hand = {}
               time.sleep(1)
-              print("Taking another card...")
+              print ("The dealer is drawing cards...")
               time.sleep(1)
-              player_cards +=1
+              #dealer draws its first card
+              dealer_cards += 1
+              dealer_hand[dealer_cards] = draw_card(card_deck)
+              dealer_points += dealer_hand[1][2]
+              #player draws its first card
+              player_cards += 1
               player_hand[player_cards] = draw_card(card_deck)
               player_points += player_hand[player_cards][2]
-              paint_card(player_hand[player_cards][0], player_hand[player_cards][1])
+              #player draws its second card
+              player_cards += 1
+              player_hand[player_cards] = draw_card(card_deck)
+              player_points += player_hand[player_cards][2]
+              #print and paint everything
+              print("Dealer:")
+              paint_card(dealer_hand[1][0], dealer_hand[1][1])
               time.sleep(1)
-              if player_points > 21:
-                print(f"\nYou have {player_points} points...")  
-                player_wins = False
-                break
-              elif player_points == 21:
-                print(f"\nYou have {player_points} points...\nThat's perfect!")
-                next_move = "S"
-                continue
-              else:
-                next_move = input(f"\nYou have {player_points} points...\nWhat's your next move? Do to draw, S to stop!\n>>>")
-                continue
-            elif next_move == "S":
-              print(f"\nYou have {player_points} points... Now let's see what the dealer gets!")
+              print("You:")
+              paint_card(player_hand[1][0], player_hand[1][1])
+              time.sleep(1)
+              paint_card(player_hand[2][0], player_hand[2][1])
+              time.sleep(1)
+              print(f"\nThe dealer has {dealer_points} points, and you have {player_points} points.")
+              #next move for the player
+              next_move = input("What is your next move? D to draw, S to stop!\n>>> ")
               while True:
-                #loop for the dealer to draw cards until he reaches 17 or more, or goes above 21
-                time.sleep(1)
-                print(f"The dealer has {dealer_points} points\nTaking another card...")
-                dealer_cards += 1
-                dealer_hand[dealer_cards] = draw_card(card_deck)
-                dealer_points += dealer_hand[dealer_cards][2]
-                time.sleep(1)
-                paint_card(dealer_hand[dealer_cards][0], dealer_hand[dealer_cards][1])
-                time.sleep(1)
-                if dealer_points < 17:
+                #3rd loop, player loop
+                if next_move not in ("D", "S"):
+                  next_move = input("Please enter a valid move! D to draw, S to stop!\n>>>")
+                  continue
+                  #continues within the player loop but will move onto the dealer loop since next_move == S
+                elif next_move == "D":
+                  time.sleep(1)
+                  print("Taking another card...")
+                  time.sleep(1)
+                  player_cards +=1
+                  player_hand[player_cards] = draw_card(card_deck)
+                  player_points += player_hand[player_cards][2]
+                  paint_card(player_hand[player_cards][0], player_hand[player_cards][1])
+                  time.sleep(1)
+                  if player_points > 21:
+                    print(f"\nYou have {player_points} points...")  
+                    player_wins = False
+                    break
+                    #breaks out of the player loop
+                  elif player_points == 21:
+                    print(f"\nYou have {player_points} points...\nThat's perfect!")
+                    next_move = "S"
                     continue
-                elif 17 <= dealer_points <= 21:
-                    print(f"The dealer has {dealer_points} points, and will stop!")
-                    if player_points > dealer_points:
+                    #continues within the player loop but will move onto the dealer loop since next_move == S
+                  else:
+                    next_move = input(f"\nYou have {player_points} points...\nWhat's your next move? Do to draw, S to stop!\n>>>")
+                    continue
+                    #continues within the player loop but will move onto the dealer loop since next_move == S
+                elif next_move == "S":
+                  print(f"\nYou have {player_points} points... Now let's see what the dealer gets!")
+                  while True:
+                    #4th loop, dealer loop (stops at 17 or more)
+                    time.sleep(1)
+                    print(f"The dealer has {dealer_points} points\nTaking another card...")
+                    dealer_cards += 1
+                    dealer_hand[dealer_cards] = draw_card(card_deck)
+                    dealer_points += dealer_hand[dealer_cards][2]
+                    time.sleep(1)
+                    paint_card(dealer_hand[dealer_cards][0], dealer_hand[dealer_cards][1])
+                    time.sleep(1)
+                    if dealer_points < 17:
+                        continue
+                        #goes into the dealer loop again
+                    elif 17 <= dealer_points <= 21:
+                        print(f"The dealer has {dealer_points} points, and will stop!")
+                        if player_points > dealer_points:
+                            player_wins = True
+                        elif player_points == dealer_points:
+                            player_wins = None
+                        else:
+                            player_wins = False
+                        break
+                        #breaks out of the dealer loop, continues within the player loop where it will also break out of
+                    elif dealer_points > 21:
                         player_wins = True
-                    elif player_points == dealer_points:
-                        player_wins = None
-                    else:
-                        player_wins = False
-                    break
-                elif dealer_points > 21:
-                    player_wins = True
-                    print(f"The dealer has {dealer_points} points!")
-                    break
-            break  
-        break
-    if player_wins == True:
-        balance = balance + int(bet)
-        print(f"You win! You've won ${bet}.\nYour balance is ${balance}.")
-    elif player_wins == False:
-        balance = balance - int(bet)
-        print(f"You lose! You've lost ${bet}.\nYour balance is ${balance}.")
-    else:
-        print("It's a tie. Your balance is still ${balance}.")
+                        print(f"The dealer has {dealer_points} points!")
+                        break
+                        #breaks out of the dealer loop, continues within the player loop where it will also break out of
+                break
+                #breaks out of the player loop, checks if we've won
+            if player_wins == True:
+                balance = balance + int(bet)
+                print(f"You win! You've won ${bet}.\nYour balance is ${balance}.")
+            elif player_wins == False:
+                balance = balance - int(bet)
+                print(f"You lose! You've lost ${bet}.\nYour balance is ${balance}.")
+            else:
+                print(f"It's a tie. Your balance is still ${balance}.")
+            break
+            #breaks out of the loop of actually playing, back into first move where we can play, quit or deposit again
 main()
