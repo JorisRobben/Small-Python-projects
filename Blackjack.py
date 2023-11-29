1f8b7e029b7b9672321b6e8d3104142c10d39d37
JorisRobben <149020196+JorisRobben@users.noreply.github.com>
2023-11-29 16:17:11 +0100

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -13,7 +13,7 @@ def generate_card_deck():
     card_deck.append(i)
   return card_deck
 
-def draw_card(card_deck, points):
+def draw_card(card_deck):
   card = int(random.choice(card_deck))
   if card in range(2,15):
     symbol = HEARTS
@@ -42,8 +42,7 @@ def draw_card(card_deck, points):
     value = 11
     sym = "A"
   card_deck.remove(card)
-  points = points + value
-  return (symbol, sym, points)
+  return (symbol, sym, value)
     
 def paint_card(symbol, sym):
     print(" ___ ")
@@ -54,18 +53,18 @@ def paint_card(symbol, sym):
 def main():
   balance = 0
   while True:
-    player_move = input("Welcome to the Blackjack table! What do you want to do? Press D to make a deposit, P to play, or Q to quit!\n>>> ")
+    player_move = input("Welcome to the Blackjack table!\nWhat do you want to do?\nPress D to make a deposit, P to play, or Q to quit!\n>>> ")
     if player_move not in ("D", "P", "Q"):
       print("Please enter valid move.")
       continue
     elif player_move == "Q":
-      print(f"Thanks for playing! Your balance is ${balance}.")
+      print(f"Thanks for playing!\nYour balance is ${balance}.")
       break
     elif player_move == "D":
       deposit = input("How much would you like to deposit (max. $1000)\n>>> ")
       if deposit.isnumeric() and  0 < int(deposit) < 1000:
         balance = balance + int(deposit)
-        print(f"Deposit accepted. Your balance is now ${balance}.")
+        print(f"Deposit accepted.\nYour balance is now ${balance}.")
         continue
       else:
         print("Please make a valid deposit.")
@@ -87,12 +86,12 @@ def main():
           time.sleep(1)
           print ("The dealer is drawing cards...")
           time.sleep(1)
-          dealer_hand[1] = draw_card(card_deck, dealer_points)
-          dealer_points = dealer_hand[1][2]
-          player_hand[1] = draw_card(card_deck, player_points)
-          player_points = player_hand[1][2]
-          player_hand[2] = draw_card(card_deck, player_points)
-          player_points = player_hand[2][2]
+          dealer_hand[1] = draw_card(card_deck)
+          dealer_points += dealer_hand[1][2]
+          player_hand[1] = draw_card(card_deck)
+          player_points += player_hand[1][2]
+          player_hand[2] = draw_card(card_deck)
+          player_points += player_hand[2][2]
           print("Dealer:")
           paint_card(dealer_hand[1][0], dealer_hand[1][1])
           time.sleep(1)
@@ -108,16 +107,19 @@ def main():
               print("Please enter a valid move!")
               continue
             elif next_move == "D":
-              player_hand[3] = draw_card(card_deck, player_points)
-              player_points = player_hand[3][2]
+              time.sleep(1)
+              print("Taking another card...")
+              time.sleep(1)
+              player_hand[3] = draw_card(card_deck)
+              player_points += player_hand[3][2]
               paint_card(player_hand[3][0], player_hand[3][1])
               if player_points > 21:
                 balance = balance - int(bet)
-                print(f"\nYou have {player_points} points... You've lost your bet! Your balance is now ${balance}.")
+                print(f"\nYou have {player_points} points... You've lost your bet!\nYour balance is now ${balance}.")
                 break
               elif player_points == 21:
                 balance = balance + int(bet)
-                print(f"\nYou have {player_points} points... You've won your bet! Your balance is now ${balance}.")
+                print(f"\nYou have {player_points} points...\nYou've won your bet!\nYour balance is now ${balance}.")
                 break
               else:
                 print(f"\nYou have {player_points} points... What is your next move? D to draw, S to stop!\n>>> ")
@@ -126,3 +128,5 @@ def main():
               print(f"\nYou have {player_points} points... Now let's see what the dealer gets!")
               break
 main()
+    
+    
